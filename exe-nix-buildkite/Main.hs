d9f1a0008e2791d1809ec915335dff40ce87882c
Drew Hess <src@drewhess.com>
2023-07-12 19:48:25 +0100

    style: reformat source using `fourmolu`
    No functional changes here, only formatting.
    
    Signed-off-by: Drew Hess <src@drewhess.com>

--- exe-nix-buildkite/Main.hs
+++ exe-nix-buildkite/Main.hs
@@ -1,34 +1,34 @@
-{-# language BlockArguments #-}
-{-# language LambdaCase #-}
-{-# language NamedFieldPuns #-}
-{-# language OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Main ( main ) where
+module Main (main) where
 
 -- algebraic-graphs
-import Algebra.Graph.AdjacencyMap ( AdjacencyMap, edge, empty, hasVertex, overlay, overlays )
-import Algebra.Graph.AdjacencyMap.Algorithm ( reachable )
+import Algebra.Graph.AdjacencyMap (AdjacencyMap, edge, empty, hasVertex, overlay, overlays)
+import Algebra.Graph.AdjacencyMap.Algorithm (reachable)
 
 -- aeson
-import Data.Aeson ( Value(..), (.=), encode, object )
+import Data.Aeson (Value (..), encode, object, (.=))
 
 -- attoparsec
-import Data.Attoparsec.Text ( parseOnly, sepBy, char, takeWhile1 )
+import Data.Attoparsec.Text (char, parseOnly, sepBy, takeWhile1)
 
 -- base
 import Data.Char
-import Data.List ( sortOn )
-import Data.Maybe ( fromMaybe, listToMaybe )
-import Data.Traversable ( for )
+import Data.List (sortOn)
+import Data.Maybe (fromMaybe, listToMaybe)
+import Data.Traversable (for)
+import System.Environment (getArgs, lookupEnv)
+import Prelude hiding (getContents, lines, readFile, words)
 import qualified Prelude
-import Prelude hiding ( getContents, lines, readFile, words )
-import System.Environment ( getArgs, lookupEnv )
 
 -- bytestring
 import qualified Data.ByteString.Lazy
 
 -- containers
-import Data.Containers.ListUtils ( nubOrd )
+import Data.Containers.ListUtils (nubOrd)
 import qualified Data.Map as Map
 
 -- filepath
@@ -38,13 +38,12 @@ import System.FilePath
 import Nix.Derivation
 
 -- process
-import System.Process hiding ( env, system )
+import System.Process hiding (env, system)
 
 -- text
-import Data.Text ( Text, isPrefixOf, pack, unpack )
+import Data.Text (Text, isPrefixOf, pack, unpack)
 import qualified Data.Text as T
-import Data.Text.IO ( readFile )
-
+import Data.Text.IO (readFile)
 
 main :: IO ()
 main = do
@@ -54,7 +53,7 @@ main = do
     cmd <- lookupEnv "POST_BUILD_HOOK"
     case cmd of
       Nothing -> return []
-      Just path -> return $ [ "--post-build-hook", path ]
+      Just path -> return $ ["--post-build-hook", path]
 
   agentTags <- do
     tags <- lookupEnv "AGENT_TAGS"
@@ -74,11 +73,11 @@ main = do
 
   -- TODO: this should be made into an option
   -- (and probably should add options for prefixing at all, using emoji, and sorting also)
-  let skipPrefix = [ "required" ]
+  let skipPrefix = ["required"]
 
   -- Run nix-instantiate on the jobs expression to instantiate .drvs for all
   -- things that may need to be built.
-  inputDrvPaths <- nubOrd . Prelude.lines <$> readProcess "nix-instantiate" [ jobsExpr ] ""
+  inputDrvPaths <- nubOrd . Prelude.lines <$> readProcess "nix-instantiate" [jobsExpr] ""
 
   -- Build an association list of a job name and the derivation that should be
   -- realised for that job.
@@ -88,45 +87,49 @@ main = do
         -- We couldn't parse the derivation to get a name, so we'll just use the
         -- derivation name.
         return (pack (takeFileName drvPath), drvPath)
-
       Right drv ->
         let name = case Map.lookup "name" (env drv) of
-                          -- There was no 'name' environment variable, so we'll just use the
-                          -- derivation name.
-                          Nothing -> pack (takeFileName drvPath)
-                          Just n -> n
-            system = if any (\x -> x `isPrefixOf` name) skipPrefix
-                     then ""
-                     else case Map.lookup "system" (env drv) of
-                            Nothing -> ""
-                            Just s -> emojify s <> ":"
-        in return (system <> name, drvPath)
+              -- There was no 'name' environment variable, so we'll just use the
+              -- derivation name.
+              Nothing -> pack (takeFileName drvPath)
+              Just n -> n
+            system =
+              if any (\x -> x `isPrefixOf` name) skipPrefix
+                then ""
+                else case Map.lookup "system" (env drv) of
+                  Nothing -> ""
+                  Just s -> emojify s <> ":"
+         in return (system <> name, drvPath)
 
   g <- foldr (\(_, drv) m -> m >>= \g -> add g drv) (pure empty) drvs
 
   let steps = map (uncurry step) drvs
         where
-          step label drvPath = (label,
-            object
-              [ "label" .= unpack label
-              , "command" .= String (pack $ unwords $ [ "nix-store" ] <> postBuildHook <> [ "-r", drvPath ])
-              , "key" .= stepify drvPath
-              , "depends_on" .= dependencies
-              ])
+          step label drvPath =
+            ( label
+            , object
+                [ "label" .= unpack label
+                , "command" .= String (pack $ unwords $ ["nix-store"] <> postBuildHook <> ["-r", drvPath])
+                , "key" .= stepify drvPath
+                , "depends_on" .= dependencies
+                ]
+            )
             where
               dependencies = map stepify $ filter (`elem` map snd drvs) $ drop 1 $ reachable drvPath g
 
-  Data.ByteString.Lazy.putStr $ encode $ object
-    [ "agents" .= agentTags
-    , "steps" .= map snd ( sortOn fst steps )
-    ]
+  Data.ByteString.Lazy.putStr $
+    encode $
+      object
+        [ "agents" .= agentTags
+        , "steps" .= map snd (sortOn fst steps)
+        ]
 
 -- Transform nix platforms into buildkite emoji
 -- See https://github.com/buildkite/emojis
 emojify :: Text -> Text
 emojify system =
-  let (_,os) = T.breakOnEnd "-" system
-  in toEmoji os <> system
+  let (_, os) = T.breakOnEnd "-" system
+   in toEmoji os <> system
   where
     toEmoji "darwin" = ":darwin: "
     toEmoji "freebsd" = ":freebsd: "
@@ -144,20 +147,17 @@ stepify = take 99 . map replace . takeBaseName
     replace '-' = '-'
     replace _ = '_'
 
-
 add :: AdjacencyMap FilePath -> FilePath -> IO (AdjacencyMap FilePath)
 add g drvPath =
-  if hasVertex drvPath g then
-    return g
+  if hasVertex drvPath g
+    then return g
+    else
+      fmap (parseOnly parseDerivation) (readFile drvPath) >>= \case
+        Left _ ->
+          return g
+        Right Derivation{inputDrvs} -> do
+          deps <- foldr (\dep m -> m >>= \g' -> add g' dep) (pure g) (Map.keys inputDrvs)
 
-  else
-    fmap (parseOnly parseDerivation) (readFile drvPath) >>= \case
-      Left _ ->
-        return g
+          let g' = overlays (edge drvPath <$> Map.keys inputDrvs)
 
-      Right Derivation{ inputDrvs } -> do
-        deps <- foldr (\dep m -> m >>= \g' -> add g' dep) (pure g) (Map.keys inputDrvs)
-
-        let g' = overlays (edge drvPath <$> Map.keys inputDrvs)
-
-        return $ overlay deps g'
+          return $ overlay deps g'
